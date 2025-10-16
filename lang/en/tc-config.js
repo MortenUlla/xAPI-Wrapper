71d136507696fe31cc8f0ad381f5418bbf81f33b
FIDL <morten@fidl.no>
2025-10-16 15:48:02 +0200

    Add files via upload

---
+++ lang/en/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://AyG5e3sCNy7S-yQW1x6lK1nGmm6d4J0I_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Module 1 (English)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
